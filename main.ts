d0abd1b27453954b055672f18f9feb988563daaa
ArcAngle <eddy@3os.mobi>
2022-08-12 14:00:38 +0200

    Test med HB 2 kort

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    hoverbit.servo_angle(-45, list_servo.S1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Butterfly)
+    hoverbit.servo_angle(0, list_servo.S1)
+    hoverbit.servo_angle(0, list_servo.S2)
+})
+input.onButtonPressed(Button.B, function () {
+    if (Direction_left) {
+        hoverbit.servo_angle(45, list_servo.S2)
+        Direction_left = 0
+    } else {
+        hoverbit.servo_angle(-45, list_servo.S2)
+        Direction_left = 1
+    }
+    basic.showLeds(`
+        # # # . .
+        # . # . .
+        # # # # .
+        # . . . #
+        # # # # .
+        `)
+})
+let Direction_left = 0
+let Direction_right = 0
+Direction_left = 0
+hoverbit.servo_angle(0, list_servo.S1)
+hoverbit.servo_angle(0, list_servo.S2)
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
 	
 })
